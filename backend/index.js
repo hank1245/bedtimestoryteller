eb32365a441874d71e6d8865255b719cc361e26e
hank <hank29206880@gmail.com>
2025-07-09 22:20:03 +0900

    feat: create dockerfile and docker-compose.yml

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: true, // 모든 origin 허용 (개발환경)
     credentials: true,
   })
 );
